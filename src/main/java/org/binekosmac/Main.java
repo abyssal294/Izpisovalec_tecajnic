2b7700fd4fd79ec026c0467fd06775ad91a307f6
abyssal294 <bine.valo@gmail.com>
2023-10-20 01:05:34 +0200

    All calls moved to JavaFX in GUI, Main starts the Application. There's a basic skeleton for the GUI. The GUI still needs to be populated with data. Missing the logic for calculating and displaying the data (as a table and as a graph).

--- src/main/java/org/binekosmac/Main.java
+++ src/main/java/org/binekosmac/Main.java
@@ -1,28 +1,10 @@
 package org.binekosmac;
 
-import org.binekosmac.database.*;
-import org.binekosmac.model.*;
-import org.binekosmac.utils.XmlDeserializer;
-
+import javafx.application.Application;
+import org.binekosmac.ui.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            DatabaseSetup.initialize();
-
-            XmlDeserializer xmlDeserializer = new XmlDeserializer();
-            DtecBS dtecBS = xmlDeserializer.processXML();
-
-            //System.out.println("Parsed object: " + dtecBS);
-
-               DatabaseSaver dbSaver = new DatabaseSaver();
-               dbSaver.save(dtecBS);
-
-            DatabaseConnection.closeConnection();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.err.println("An error occured: " +e.getMessage());
-            }
+        Application.launch(GUI.class, args);
     }
 }
--- src/main/java/org/binekosmac/ui/GUI.java
+++ src/main/java/org/binekosmac/ui/GUI.java
@@ -1,11 +1,16 @@
 package org.binekosmac.ui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import org.binekosmac.database.DatabaseSaver;
+import org.binekosmac.database.DatabaseSetup;
+import org.binekosmac.model.DtecBS;
+import org.binekosmac.utils.XmlDeserializer;
 
 public class GUI extends Application {
 
@@ -14,7 +19,7 @@ public class GUI extends Application {
         VBox rootLayout = new VBox(10);
         rootLayout.setPadding(new Insets(10));
 
-        Label titleLabel = new Label("Izpis tečajnic");
+        Label titleLabel = new Label("Prikazovalec tečajev");
         rootLayout.getChildren().add(titleLabel);
 
         HBox dateLayout = new HBox(10);
@@ -49,8 +54,35 @@ public class GUI extends Application {
         mainLayout.setPadding(new Insets(10));
 
         Scene scene = new Scene(mainLayout, 800, 400);
-        primaryStage.setTitle("Valute");
+        primaryStage.setTitle("Izpis tečajnic");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        Label loadingLabel = new Label("Pripravljam podatke...");
+        rootLayout.getChildren().add(loadingLabel);
+
+        new Thread(() -> {
+            try {
+                DatabaseSetup.initialize();
+                XmlDeserializer xmlDeserializer = new XmlDeserializer();
+                DtecBS dtecBS = xmlDeserializer.processXML();
+                DatabaseSaver dbSaver = new DatabaseSaver();
+                dbSaver.save(dtecBS);
+
+                // If we're here, data is loaded. Update the GUI on the JavaFX Application Thread
+                Platform.runLater(() -> {
+                    loadingLabel.setText("Podatki so pripravljeni!"); // Indicate that data is ready
+                    // You can also initiate actions that should happen after loading is complete
+                    // For instance, populating the currencyListView or other UI elements
+                });
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                // Handle exceptions, e.g., show a dialog with the error, log the error, etc.
+                Platform.runLater(() -> {
+                    loadingLabel.setText("Napaka pri pripravi podatkov!"); // Indicate that there was an error
+                });
+            }
+        }).start();
     }
 }
