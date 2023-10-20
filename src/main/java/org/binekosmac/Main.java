0c1a8c347a93e8db61095bad03c4b2c87ddd664b
abyssal294 <bine.valo@gmail.com>
2023-10-20 22:21:31 +0200

    Finished exchange rate list for user to choose off.

--- src/main/java/org/binekosmac/Main.java
+++ src/main/java/org/binekosmac/Main.java
@@ -3,8 +3,12 @@ package org.binekosmac;
 import javafx.application.Application;
 import org.binekosmac.ui.GUI;
 
+
 public class Main {
     public static void main(String[] args) {
         Application.launch(GUI.class, args);
     }
+
 }
+
+
--- src/main/java/org/binekosmac/ui/GUI.java
+++ src/main/java/org/binekosmac/ui/GUI.java
@@ -7,10 +7,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import org.binekosmac.database.DatabaseConnection;
 import org.binekosmac.database.DatabaseSaver;
 import org.binekosmac.database.DatabaseSetup;
 import org.binekosmac.model.DtecBS;
 import org.binekosmac.utils.XmlDeserializer;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
 
 public class GUI extends Application {
 
@@ -31,6 +38,12 @@ public class GUI extends Application {
         ListView<String> currencyListView = new ListView<>();  // Will be populated with currencies later
         currencyListView.setPrefHeight(150);
         rootLayout.getChildren().add(currencyListView);
+        currencyListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        List<String> currencies = Arrays.asList("Ameriški dolar (USD)", "Japonski jen - JPY)", "Bolgarski lev - BGN", "Češka krona - CZK", "Danska krona - DKK", "Britanski funt - GBP", "Madžarski forint - HUF", "Poljski zlot - PLN", "Romunski lev - RON", "Švedska krona - SEK",
+                "Islandska krona - ISK", "Švicarski frank - CHF", "Norveška krona - NOK", "Turška lira - TRY", "Avstralski dolar - AUD", "Kanadski dolar - CAD", "Mehiški peso - MXN (z 2.1.2008)", "Kitajski juan renminbi - CNY", "Hongkonški dolar - HKD", "Indijska rupija - INR (z 2.1.2009)", "Indonezijska rupija - IDR", "Izraelski šakel - ILS", "Južnokorejski von - KRW",
+                "Malezijski ringit - MYR", "Novozelandski dolar - NZD", "Filipinski peso - PHP", "Singapurski dolar - SGD", "Tajski baht - THB", "Južnoafriški rand - ZAR", "Ciprski funt - CYP (nadomestil evro 1.1.2008)", "Slovaška krona - SKK (nadomestil evro 1.1.2009)", "Ruski rubelj - RUB (ni podatkov po 1.3.2022)",
+                "Malteška lira - MTL (nadomestil evro 1.1.2008)", "Latvijski lats - LVL(nadomestil evro 1.1.2014)", "Litvanski litas - LTL (nadomestil evro 1.1.2015)", "Hrvaška kuna - HRK (nadomestil evro 1.1.2023)", "Estonska krona - EEK (nadomestil evro 1.1.2011)");
+        currencyListView.getItems().addAll(currencies);
 
         Button prikazButton = new Button("Prikaz");
         rootLayout.getChildren().add(prikazButton);
@@ -69,6 +82,10 @@ public class GUI extends Application {
                 DatabaseSaver dbSaver = new DatabaseSaver();
                 dbSaver.save(dtecBS);
 
+                displayData(); // TODO: na koncu odstrani!
+
+
+
                 // If we're here, data is loaded. Update the GUI on the JavaFX Application Thread
                 Platform.runLater(() -> {
                     loadingLabel.setText("Podatki so pripravljeni!"); // Indicate that data is ready
@@ -84,5 +101,29 @@ public class GUI extends Application {
                 });
             }
         }).start();
+    }
+    // TODO: na koncu odstrani!
+    public static void displayData() {
+        // SQL query to retrieve the first 50 records from the 'tecajnice' table
+        String query = "SELECT * FROM tecajnice LIMIT 50";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String datum = rs.getString("datum");
+                String oznaka = rs.getString("oznaka");
+                int sifra = rs.getInt("sifra");
+                double vrednost = rs.getDouble("vrednost");
+
+                System.out.format("ID: %d, Datum: %s, Oznaka: %s, Sifra: %d, Vrednost: %f\n", id, datum, oznaka, sifra, vrednost);
+            }
+        } catch (Exception e) {
+            System.err.println("Error querying the database: " + e.getMessage());
+            // Optional: Print stack trace for debugging
+            e.printStackTrace();
+        }
     }
 }
