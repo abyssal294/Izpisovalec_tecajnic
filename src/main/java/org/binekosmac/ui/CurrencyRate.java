febe0a8e8b71771271f758117d0120eafc908778
abyssal294 <bine.valo@gmail.com>
2023-10-22 14:01:19 +0200

    Just before rounding "vrednost" for table

--- src/main/java/org/binekosmac/ui/CurrencyRate.java
+++ src/main/java/org/binekosmac/ui/CurrencyRate.java
@@ -5,6 +5,7 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class CurrencyRate {
