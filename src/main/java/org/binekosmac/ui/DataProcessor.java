9497634741b536291ef37ad29ea42528dd93e597
abyssal294 <bine.valo@gmail.com>
2023-10-22 13:19:55 +0200

    Forex trading calculation now functional. Next: incorporating the graph. Packaging the app.

--- src/main/java/org/binekosmac/ui/DataProcessor.java
+++ src/main/java/org/binekosmac/ui/DataProcessor.java
@@ -2,25 +2,28 @@ package org.binekosmac.ui;
 
 import org.binekosmac.database.DatabaseConnection;
 
+import java.math.BigDecimal;
 import java.sql.*;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.math.RoundingMode;
 
 public class DataProcessor {
     public List<CurrencyRate> retrieveData(LocalDate startDate, LocalDate endDate, List<String> currencies) throws SQLException {
-            StringBuilder queryBuilder = new StringBuilder("SELECT * FROM tecajnice WHERE datum BETWEEN ? AND ? AND oznaka IN (");
+        StringBuilder queryBuilder = new StringBuilder("SELECT * FROM tecajnice WHERE datum BETWEEN ? AND ? AND oznaka IN (");
 
-            for (int i = 0; i < currencies.size(); i++) {
-              queryBuilder.append("?");
-              if (i < currencies.size() - 1) {
+        for (int i = 0; i < currencies.size(); i++) {
+            queryBuilder.append("?");
+            if (i < currencies.size() - 1) {
                 queryBuilder.append(",");
-              }
             }
-            queryBuilder.append(")");
-            queryBuilder.append("ORDER BY oznaka");
+        }
+        queryBuilder.append(")");
+        queryBuilder.append("ORDER BY oznaka");
 
-            Connection connection = DatabaseConnection.getConnection();
+        Connection connection = DatabaseConnection.getConnection();
 
         PreparedStatement statement = connection.prepareStatement(queryBuilder.toString());
 
@@ -28,8 +31,8 @@ public class DataProcessor {
         statement.setDate(2, Date.valueOf(endDate));
 
         int index = 3;
-        for(String currency : currencies) {
-          statement.setString(index++, currency);
+        for (String currency : currencies) {
+            statement.setString(index++, currency);
         }
 
         List<CurrencyRate> rates = new ArrayList<>();
@@ -47,6 +50,81 @@ public class DataProcessor {
         return rates;
     }
 
+    public String calculateForexData(String currency1, String currency2, String timeFrame) {
+        try {
+            // Današnji dan (dinamično) in glede na le-tega, izračun začetnega datuma
+              LocalDate today = LocalDate.now();
+              LocalDate historicalDate = calculateStartDate(today, timeFrame);
 
+            // SQL string za iskanje vrednosti (modificirano za iskanje zadnje vrednosti, če za izbrano ni podatka)
+            String queryTemplate =
+                    "SELECT vrednost FROM tecajnice WHERE oznaka = ? AND datum<=?ORDER BY datum DESC LIMIT 1";
+
+            try (Connection connection = DatabaseConnection.getConnection()) {
+                // Prva vrednost v časovnem obdobju za prvo valuto
+                BigDecimal initialCurrency1 = fetchCurrencyValue(connection, queryTemplate, currency1, historicalDate);
+
+                // Današnja vrendnost za prvo valuto
+                BigDecimal finalCurrency1 = fetchCurrencyValue(connection, queryTemplate, currency1, today);
+
+                // Prva vrednost v časovnem obdobju za drugo valuto
+                BigDecimal initialCurrency2 = fetchCurrencyValue(connection, queryTemplate, currency2, historicalDate);
+
+                // Današnja vrednost za drugo valuto
+                BigDecimal finalCurrency2 = fetchCurrencyValue(connection, queryTemplate, currency2, today);
+
+                // Preverimo, da so vse vrednosti na voljo
+                if (initialCurrency1 == null || finalCurrency1 == null || initialCurrency2 == null || finalCurrency2 == null) {
+                    return "Ni podatkov za izbrane parametre. Ponovno zaženite program in poskusite znova.";
+                }
+
+                // Izračun razmerja med valutama
+                BigDecimal historicalRatio = initialCurrency1.divide(initialCurrency2, 8, RoundingMode.HALF_UP);
+                BigDecimal recentRatio = finalCurrency1.divide(finalCurrency2, 8, RoundingMode.HALF_UP);
+
+                BigDecimal change = recentRatio.subtract(historicalRatio).divide(historicalRatio, 8, RoundingMode.HALF_UP);
+                BigDecimal percentageChange = change.multiply(new BigDecimal("100"));
+
+                percentageChange = percentageChange.setScale(4, RoundingMode.HALF_UP);
+
+                // Izpis rezultata
+                return String.format("%s se je glede na %s spremenil: %s%%", currency1, currency2, percentageChange.toPlainString());
+            }
+        } catch (SQLException e) {
+            // Error handling
+            e.printStackTrace();
+            return "Pri pripravi podatkov je prišlo do napake. Prosim, da znova zaženete program.";
+        }
+    }
+
+    private LocalDate calculateStartDate(LocalDate endDate, String timeFrame) {
+        // Izračun časovnega obdobja
+        switch (timeFrame) {
+            case "1 teden":
+                return endDate.minus(1, ChronoUnit.WEEKS);
+            case "1 mesec":
+                return endDate.minus(1, ChronoUnit.MONTHS);
+            case "6 mesecev":
+                return endDate.minus(6, ChronoUnit.MONTHS);
+            case "1 leto":
+                return endDate.minus(1, ChronoUnit.YEARS);
+            default:
+                throw new IllegalArgumentException("Težava pri izbiri časovnega obdobja.");
+        }
+    }
+    // Metoda za pridobitev vrednosti iz baze
+    private BigDecimal fetchCurrencyValue(Connection connection, String query, String currency, LocalDate date) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, currency);
+            statement.setDate(2, Date.valueOf(date));
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getBigDecimal("vrednost");
+                }
+            }
+        }
+        return null;
+    }
 }
 
--- src/main/java/org/binekosmac/ui/GUI.java
+++ src/main/java/org/binekosmac/ui/GUI.java
@@ -18,11 +18,13 @@ import org.binekosmac.model.DtecBS;
 import org.binekosmac.utils.XmlDeserializer;
 
 
+import javax.xml.crypto.Data;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 
 public class GUI extends Application {
@@ -54,7 +56,7 @@ public class GUI extends Application {
         currencyListView.setPrefHeight(150);
         rootLayout.getChildren().add(currencyListView);
         currencyListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        List<String> currencies = Arrays.asList("Ameriški dolar USD", "Japonski jen - JPY", "Bolgarski lev - BGN",
+        List<String> currencies = Arrays.asList("Ameriški dolar - USD", "Japonski jen - JPY", "Bolgarski lev - BGN",
                 "Češka krona - CZK", "Danska krona - DKK", "Britanski funt - GBP", "Madžarski forint - HUF",
                 "Poljski zlot - PLN", "Romunski lev - RON", "Švedska krona - SEK",
                 "Islandska krona - ISK", "Švicarski frank - CHF", "Norveška krona - NOK", "Turška lira - TRY",
@@ -133,7 +135,7 @@ public class GUI extends Application {
 
         });
 
-        // Dodatna funkcionalnost
+        // Dodatna funkcionalnost: izračun oportuninetnih zaslužkov/izgub
         VBox rightLayout = new VBox(10);
         rightLayout.getChildren().add(new Label("Izračun oportunitetnih zaslužkov/izgub"));
         List<String> forexCurrencies = Arrays.asList("USD", "JPY", "BGN", "CZK", "DKK", "GBP", "HUF",
@@ -144,27 +146,49 @@ public class GUI extends Application {
 
         Label currency1Label = new Label("Izberite željeno valuto:");
         Label currency2Label = new Label("Izberite drugo valuto za primerjavo:");
+
         ComboBox<String> currencyDropdown1 = new ComboBox<>();
         currencyDropdown1.getItems().addAll(forexCurrencies);
         currencyDropdown1.getSelectionModel().selectFirst();
+
         ComboBox<String> currencyDropdown2 = new ComboBox<>();
         currencyDropdown2.getItems().addAll(forexCurrencies);
         currencyDropdown2.getSelectionModel().select(1);
+
         rightLayout.getChildren().addAll(currency1Label, currencyDropdown1, currency2Label, currencyDropdown2);
 
         Label timeRangeLabel = new Label("Izberite časovno obdobje:");
         ComboBox<String> timeRangeDropdown = new ComboBox<>();
         timeRangeDropdown.getItems().addAll("1 teden", "1 mesec", "6 mesecev", "1 leto");
+
         rightLayout.getChildren().addAll(timeRangeLabel, timeRangeDropdown);
 
-        Label resultLabel = new Label("Rezultat tukaj");
-        rightLayout.getChildren().add(resultLabel);
+        Button calculateButton = new Button("Izračunaj");
+
+        Label resultLabel = new Label("Tukaj se bo izpisal izračun");
+        rightLayout.getChildren().addAll(calculateButton, resultLabel);
+
+
+        calculateButton.setOnAction(event -> {
+            String selectedCurrency1 = currencyDropdown1.getSelectionModel().getSelectedItem();
+            String selectedCurrency2 = currencyDropdown2.getSelectionModel().getSelectedItem();
+            String selectedTimeFrame = timeRangeDropdown.getSelectionModel().getSelectedItem();
+
+            // Check if all selections are made
+            if (selectedCurrency1 != null && selectedCurrency2 != null && selectedTimeFrame != null) {
+                DataProcessor dataProcessor = new DataProcessor();
+                String result = dataProcessor.calculateForexData(selectedCurrency1, selectedCurrency2, selectedTimeFrame);
+                resultLabel.setText(result);
+            } else {
+                resultLabel.setText("Izberite časovno obdobje.");
+            }
+        });
 
         //Nastavitev za layout
         HBox mainLayout = new HBox(20, rootLayout, rightLayout);
         mainLayout.setPadding(new Insets(10));
 
-        Scene scene = new Scene(mainLayout, 800, 400);
+        Scene scene = new Scene(mainLayout, 1000, 600);
         primaryStage.setTitle("Izpis tečajnic");
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
@@ -190,7 +214,7 @@ public class GUI extends Application {
                 e.printStackTrace();
                 // Handle exceptions, e.g., show a dialog with the error, log the error, etc.
                 Platform.runLater(() -> {
-                    loadingLabel.setText("Napaka pri pripravi podatkov!");
+                    loadingLabel.setText("Napaka pri pripravi podatkov! Ponovno zaženite program.");
                 });
             }
         }).start();
