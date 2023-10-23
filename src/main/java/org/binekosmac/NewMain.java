db1437972b5608e0bae23bdecdadd5e57b8c2a89
abyssal294 <123805003+abyssal294@users.noreply.github.com>
2023-10-23 03:23:20 +0200

    comment

--- src/main/java/org/binekosmac/NewMain.java
+++ src/main/java/org/binekosmac/NewMain.java
@@ -1,5 +1,5 @@
 package org.binekosmac;
-
+// Vzpostavljeno samo, ker ima nemodularni JavaFX svoje muhe.
 public class NewMain {
     public static void main(String[] args) {
         Main.main(args);
